b5028ce040f79295d4993efb6db9132f3cd3fda7
EtoMisha <baikov.mv@yandex.ru>
2023-01-14 15:55:34 +0300

    Инсталляция

---
+++ install/install-linux.sh
@@ -0,0 +1,1 @@
+jpackage --name SmartCalc --input . --main-jar SmartCalc.jar --resource-dir package/linux --type deb --app-version 3.0 --linux-shortcut --linux-menu-group "SmartCalc"
--- install/install.sh
+++ install/install.sh
@@ -1,2 +1,5 @@
 jpackage --name SmartCalc --input . --main-jar SmartCalc.jar --resource-dir package/macos --type pkg --app-version 3.0
+jpackage --name SmartCalc --input . --main-jar SmartCalc.jar --resource-dir package/linux --type deb --app-version 3.0 --linux-shortcut --linux-menu-group "SmartCalc"
+jpackage --name SmartCalc --input . --main-jar SmartCalc.jar --resource-dir package/macos --type exe --app-version 3.0 --win-dir-chooser --win-menu --win-menu-group "SmartCalc"
+
 open SmartCalc-3.0.pkg
